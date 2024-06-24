98053d3249b3d269b4f8b6a99d9251e030a63f00
Natalie <133613092+natccc@users.noreply.github.com>
2024-06-24 22:08:12 +0100

    Time: 665 ms (97.83%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,15 @@
-SELECT 
-q1.person_name
-FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
-GROUP BY q1.turn
-HAVING sum(q2.weight)<=1000
-order by sum(q2.weight) DESC
+-- SELECT 
+-- q1.person_name
+-- FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+-- GROUP BY q1.turn
+-- HAVING sum(q2.weight)<=1000
+-- order by sum(q2.weight) DESC
+-- LIMIT 1
+
+
+SELECT person_name
+FROM
+(SELECT *, sum(weight) OVER (ORDER BY turn) AS sum_weight FROM queue) t
+WHERE sum_weight<=1000
+ORDER BY sum_weight DESC
 LIMIT 1
