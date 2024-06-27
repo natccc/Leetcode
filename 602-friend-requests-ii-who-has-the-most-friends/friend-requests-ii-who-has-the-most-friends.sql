83b79d3430b75ed1828d431e82efc1004646a912
Natalie <133613092+natccc@users.noreply.github.com>
2024-06-27 16:43:21 +0100

    Time: 301 ms (78.64%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -3,17 +3,15 @@ WITH combined AS
 (SELECT requester_id AS id FROM RequestAccepted 
 UNION ALL
 SELECT accepter_id AS id FROM RequestAccepted
-),
-id_count AS(
+)
+
 SELECT id, COUNT(id) AS num
 FROM combined
-GROUP BY id)
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
 
-SELECT * from id_count
-WHERE num IN(
-    SELECT MAX(num)
-    FROM id_count
-)
+
 
 
 
